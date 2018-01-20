9372e9e1a0b42c67d8039ae61179a70f2ba0fbcc
Diego Lopes <diego.lopes.2402@gmail.com>
2018-01-20 04:11:22 -0200

    Update RPC Server

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -340,11 +340,12 @@ static const CRPCCommand vRPCCommands[] =
 
     /* Private Send */
 
-    /*{ "getnewprivateaddress",   &getnewprivateaddress,   false,     false,     true },
+    /*
      */
     /*{ "scanforprivatetxns",     &scanforprivatetxns,     false,     false,     false }, */
     /*{ "importprivateaddress",   &importprivateaddress,   false,     false,     true },
     */
+    { "getnewprivateaddress",   &getnewprivateaddress,   false,     false,     true },
     { "listprivateaddresses",   &listprivateaddresses,   false,     false,     true },
     { "sendtoprivateaddress",   &sendtoprivateaddress,   false,     false,     true },
 
